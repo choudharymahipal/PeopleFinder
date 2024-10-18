abc9f1608059c428d501ffccba46c09efe570da8
choudharymahipal <mschoudhary81@gmail.com>
2024-10-18 16:30:14 +0530

    theme setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginComponent } from './Shared/Components/login/login.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { ChangePasswordComponent } from './Components/change-password/change-password.component';
+import { ViewResultComponent } from './Components/view-result/view-result.component';
+import { SettingsComponent } from './Components/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'change-password', component: ChangePasswordComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'view', component: ViewResultComponent },
+  { path: '**', redirectTo: '',pathMatch:'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Shared/Components/header/header.component';
+import { FooterComponent } from './Shared/Components/footer/footer.component';
+import { LoginComponent } from './Shared/Components/login/login.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AuthService } from './Shared/Services/auth.service';
+import { CommonService } from './Shared/Services/common.service';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { SettingsComponent } from './Components/settings/settings.component';
+import { ChangePasswordComponent } from './Components/change-password/change-password.component';
+import { SearchResultComponent } from './Components/search-result/search-result.component';
+import { ViewResultComponent } from './Components/view-result/view-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    ProfileComponent,
+    SettingsComponent,
+    ChangePasswordComponent,
+    SearchResultComponent,
+    ViewResultComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [AuthService, CommonService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
