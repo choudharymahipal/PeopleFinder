4814dd41010416da31a4cbdd28f0eaf207b0597c
choudharymahipal <mschoudhary81@gmail.com>
2024-10-22 17:48:42 +0530

    profile img

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import {environment} from './../.././../environments/environment';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-
+  userImgUrl= environment.userProfile;
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  userProfile:
+    'https://raw.githubusercontent.com/choudharymahipal/choudharymahipal/4a33fde504be30cd597bbc4b03dc9a4630483a70/assets/images/users/user.jpg',
+};
